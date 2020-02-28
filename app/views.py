77830fdbff12c4cac8d41988c53f94286a9f6105
Leslie Cheng <leslie.cheng5@gmail.com>
2020-02-27 19:51:12 -0800

    Heartbeat.

--- app/views.py
+++ app/views.py
@@ -5,13 +5,12 @@ import logging
 
 class RunnerAPI(views.MethodView):
     def get(self):
-        return ("Hello world", 200)
+        return ("Heartbeat", 200)
 
     def post(self):
         data = request.get_json(silent=True)
         logging.info(data)
         if data and "language" in data and "code" in data:
-            logging.info(data)
             runner = CodeRunner(data["language"], data["code"])
             output = runner.run()
             return (output, 200)
